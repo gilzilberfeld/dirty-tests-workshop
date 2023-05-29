829423954e32f2ef03ba93ef57b2ae4c6cdaa554
gilzilberfeld <gil.zilberfeld@gmail.com>
2023-05-29 15:17:08 +0300

    python ready

--- pythonProject/test2.py
+++ pythonProject/test2.py
@@ -14,41 +14,50 @@ from selenium.webdriver.common.by import By
 from chromedriver_py import binary_path
 from App.Calculator import Calculator
 
-
-path_to_file =os.getcwd() + ("\Resources\slow_unit_test_data.csv")
 path_to_file1 =os.getcwd() + ("\Resources\sequence_api_data.csv")
 path_to_file2 =os.getcwd() + ("\Resources\slow_unit_test_data_2.csv")
 
-def test_1_2_3():
-    c = Calculator()
-    c.press("1")
-    c.press("2")
-    c.press("3")
-    assert c.getDisplay() == "123"
+
+def get_test_data1():
+    with open(path_to_file1, 'r') as f:
+        reader = csv.reader(f)
+        data = {tuple(row) for row in reader}
+    return data
 
 
-# @pytest.mark.skip(reason="no way of currently testing this")
-def test_plus_and_2():
+@pytest.mark.parametrize("input, output", get_test_data1())
+def test_multiple_sequences(input, output):
     conn = http.client.HTTPConnection('localhost',8080)
 
     conn.request("POST", "/calc/reset")
     conn.getresponse().read()
 
-    conn.request("POST", "/calc/press?key=" + urllib.parse.quote('+'))
-    conn.getresponse().read()
+    message = SequenceMessageHelper()
+    message.version = "1.0";
+    message.addSequence(input);
+    message.resetOnError = True;
 
-    conn.request("POST", "/calc/press?key=" + urllib.parse.quote('2'))
-    conn.getresponse().read()
+    headers = {"Accept": "application/json"}
+    msgJSON = jsonpickle.encode(message, unpicklable=False)
 
+    conn.request("POST", "/calc/sequence", body=msgJSON, headers=headers)
+    conn.getresponse().read()
 
     conn.request("GET", "/calc/display")
     response = conn.getresponse()
     result_json = json.loads(response.read())
 
-    assert result_json['display'] == "2"
+    assert result_json['display'] == output
+
     conn.close()
 
-def test_ui_1_plus_34():
+def test_01():
+    c = Calculator()
+    c.press("0")
+    c.press("1")
+    assert (c.getDisplay() == "1") == True
+
+def test_ui_123():
     options = webdriver.ChromeOptions()
     options.add_argument("start-remote-allow-origins=*")
 
@@ -67,7 +76,7 @@ def test_ui_1_plus_34():
     keyElement3.click()
     time.sleep(0.5)
 
-    BUTTON_XPATH2 = "//*[@id=\'id" + "plus" + "\']"
+    BUTTON_XPATH2 = "//*[@id=\'id" + "2" + "\']"
     keyElement2 = driver.find_element(By.XPATH, BUTTON_XPATH2)
     keyElement2.click()
     time.sleep(0.5)
@@ -77,88 +86,60 @@ def test_ui_1_plus_34():
     keyElement1.click()
     time.sleep(0.5)
 
-    BUTTON_XPATH = "//*[@id=\'id" + "4" + "\']"
-    keyElement = driver.find_element(By.XPATH, BUTTON_XPATH)
-    keyElement.click()
-    time.sleep(0.5)
-
     BUTTON_XPATH5 = "//*[@id=\'id" + "Update" + "\']"
     keyElement5 = driver.find_element(By.XPATH, BUTTON_XPATH5)
     keyElement5.click()
-    time.sleep(0.5)
+    time.sleep(5)
 
     DISPLAY_XPATH = "//*[@id=\"result\"]";
     res = driver.find_element(By.XPATH, DISPLAY_XPATH)
     result = res.get_attribute("value")
 
-    assert result == "34"
+    assert result == "123"
     driver.quit()
 
 
-def get_test_data():
-    with open(path_to_file, 'r') as f:
+def get_test_data2():
+    with open(path_to_file1, 'r') as f:
         reader = csv.reader(f)
         data = {tuple(row) for row in reader}
     return data
 
-@pytest.mark.parametrize("input, output", get_test_data())
-def test_multiple_vals(input, output):
-    c = Calculator()
-    for ch in input:
-        c.press(ch)
-    assert c.getDisplay() == output
-
-
-def test_nothing():
-    c = Calculator()
-    assert (c.getDisplay() == "0") == True
-
-
-# @pytest.mark.skip(reason="no way of currently testing this")
-def test_1_and_plus():
+@pytest.mark.parametrize("input, output", get_test_data2())
+def test_more_sequences(input, output):
     conn = http.client.HTTPConnection('localhost',8080)
 
     conn.request("POST", "/calc/reset")
     conn.getresponse().read()
 
-    conn.request("POST", "/calc/press?key=" + urllib.parse.quote('1'))
+    message = SequenceMessageHelper()
+    message.version = "1.0";
+    message.addSequence(input);
+    message.resetOnError = True;
+
+    headers = {"Accept": "application/json"}
+    msgJSON = jsonpickle.encode(message, unpicklable=False)
+
+    conn.request("POST", "/calc/sequence", body=msgJSON, headers=headers)
     conn.getresponse().read()
 
     conn.request("GET", "/calc/display")
     response = conn.getresponse()
     result_json = json.loads(response.read())
 
-    assert result_json['display'] == "1"
+    assert result_json['display'] == output
+
     conn.close()
 
 
-def test_1():
+def test_5_mul_and_mul():
     c = Calculator()
-    c.press("1")
-    assert  (c.getDisplay() == "1") == True
+    c.press("5")
+    c.press("*");
+    c.press("*");
+    assert c.getDisplay() ==  "5"
 
-
-
-def test_zero_zero():
-    conn = http.client.HTTPConnection('localhost',8080)
-
-    conn.request("POST", "/calc/reset")
-    conn.getresponse().read()
-
-    conn.request("POST", "/calc/press?key=" + urllib.parse.quote('0'))
-    conn.getresponse().read()
-    conn.request("POST", "/calc/press?key=" + urllib.parse.quote('0'))
-    conn.getresponse().read()
-
-
-    conn.request("GET", "/calc/display")
-    response = conn.getresponse()
-    result_json = json.loads(response.read())
-
-    assert result_json['display'] == "0"
-    conn.close()
-
-def test_div_6_2():
+def test_5_plus_3():
     options = webdriver.ChromeOptions()
     options.add_argument("start-remote-allow-origins=*")
 
@@ -172,30 +153,25 @@ def test_div_6_2():
     keyElement4.click()
     time.sleep(0.5)
 
-    BUTTON_XPATH3 = "//*[@id=\'id" + "6" + "\']"
+    BUTTON_XPATH3 = "//*[@id=\'id" + "5" + "\']"
     keyElement3 = driver.find_element(By.XPATH, BUTTON_XPATH3)
     keyElement3.click()
     time.sleep(0.5)
 
-    BUTTON_XPATH2 = "//*[@id=\'id" + "div" + "\']"
+    BUTTON_XPATH2 = "//*[@id=\'id" + "plus" + "\']"
     keyElement2 = driver.find_element(By.XPATH, BUTTON_XPATH2)
     keyElement2.click()
     time.sleep(0.5)
 
-    BUTTON_XPATH1 = "//*[@id=\'id" + "2" + "\']"
+    BUTTON_XPATH1 = "//*[@id=\'id" + "3" + "\']"
     keyElement1 = driver.find_element(By.XPATH, BUTTON_XPATH1)
     keyElement1.click()
-    time.sleep(0.5)
-
-    BUTTON_XPATH = "//*[@id=\'id" + "eql" + "\']"
-    keyElement = driver.find_element(By.XPATH, BUTTON_XPATH)
-    keyElement.click()
     time.sleep(0.5)
 
     BUTTON_XPATH5 = "//*[@id=\'id" + "Update" + "\']"
     keyElement5 = driver.find_element(By.XPATH, BUTTON_XPATH5)
     keyElement5.click()
-    time.sleep(0.5)
+    time.sleep(5)
 
     DISPLAY_XPATH = "//*[@id=\"result\"]";
     res = driver.find_element(By.XPATH, DISPLAY_XPATH)
@@ -204,24 +180,41 @@ def test_div_6_2():
     assert result == "3"
     driver.quit()
 
+def get_test_data2():
+    with open(path_to_file2, 'r') as f:
+        reader = csv.reader(f)
+        data = {tuple(row) for row in reader}
+    return data
 
-def test_1_and_C():
+@pytest.mark.parametrize("input, output", get_test_data2())
+def test_multiple_vals_2(input, output):
     c = Calculator()
-    c.press("1")
-    c.press("C")
-    assert c.getDisplay() == "0"
+    c.pressAll(input)
+    assert c.getDisplay() == output
 
-
-def test_0():
+def test_result():
     conn = http.client.HTTPConnection('localhost',8080)
 
-    conn.request("POST", "/calc/press?key=" + urllib.parse.quote('0'))
+    conn.request("POST", "/calc/reset")
+    conn.getresponse().read()
+
+    message = SequenceMessageHelper()
+    message.version = "1.0";
+    message.addSequence("5+2C");
+    message.resetOnError = True;
+
+    headers = {"Accept": "application/json"}
+    msgJSON = jsonpickle.encode(message, unpicklable=False)
+
+    conn.request("POST", "/calc/sequence", body=msgJSON, headers=headers)
     conn.getresponse().read()
 
     conn.request("GET", "/calc/display")
     response = conn.getresponse()
-    result_json = json.loads(response.read())
+    result_json = response.read().decode("utf-8")
 
-    assert result_json['display'] == "0"
-    conn.close()
+    ref_file = os.getcwd() + "\Resources\\reference_result.json"
+    with open(ref_file, 'r') as f:
+        json_reference = f.read()
 
+    assert  result_json == json_reference
