45d0e6427971a9fb831a90741dd04e46bc5a7449
gilzilberfeld <gil.zilberfeld@gmail.com>
2023-06-03 12:44:03 +0300

    slowtests

--- JS/DirtyTestsTS/src/temp/unit.test.ts
+++ JS/DirtyTestsTS/src/temp/unit.test.ts
@@ -1,36 +1,40 @@
 import { Calculator } from "../App/Calculator";
 
-test('test_nothing', ()=>{
+describe("unit tests", () => {
 
-    const c = new Calculator(); 
-    
-    expect(c.getDisplay()).toEqual("0");
-});
+    test('test_nothing', () => {
 
-test('test_1', ()=> {
-    const c = new Calculator(); 
-    c.press("1")
-    expect(c.getDisplay()).toEqual("1");
-});
+        const c = new Calculator();
 
-test('test_1_and_C', ()=> {
-    const c = new Calculator(); 
-    c.press("1")
-    c.press("C")
-    expect(c.getDisplay()).toEqual("0");
-});
+        expect(c.getDisplay()).toEqual("0");
+    });
 
-test('test_01', ()=> {
-    const c = new Calculator(); 
-    c.press("0")
-    c.press("1")
-    expect(c.getDisplay()).toEqual("1");
-});
+    test('test_1', () => {
+        const c = new Calculator();
+        c.press("1")
+        expect(c.getDisplay()).toEqual("1");
+    });
 
-test('test_5_mul_and_mul', ()=> {
-    const c = new Calculator(); 
-    c.press("5")
-    c.press("*")
-    c.press("*")
-    expect(c.getDisplay()).toEqual("5");
+    test('test_1_and_C', () => {
+        const c = new Calculator();
+        c.press("1")
+        c.press("C")
+        expect(c.getDisplay()).toEqual("0");
+    });
+
+    test('test_01', () => {
+        const c = new Calculator();
+        c.press("0")
+        c.press("1")
+        expect(c.getDisplay()).toEqual("1");
+    });
+
+    test('test_5_mul_and_mul', () => {
+        const c = new Calculator();
+        c.press("5")
+        c.press("*")
+        c.press("*")
+        expect(c.getDisplay()).toEqual("5");
+    });
+
 });
